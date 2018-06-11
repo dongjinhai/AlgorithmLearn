f5a5423c64ce977d632f27bcac8dab13bfb38d42
dongdawang <djhsoksolars@gmail.com>
2018-06-11 10:07:45 +0800

    bug fix
    fix select_sort

--- select_sort.py
+++ select_sort.py
@@ -1,10 +1,19 @@
 
-def selectSort(origin_list):
-	for i in range(len(origin_list)):
-		for j in range(i+1,len(origin_list)):
-			if origin_list[i] > origin_list[j]:
-				origin_list[i],origin_list[j] = origin_list[j],origin_list[i]
-	return origin_list
+"""选择排序是每次将最小（大）的元素找出来，和查找列表的最左（右）的元素交换值
+和冒泡排序的区别是冒泡排序一趟中可能要交换几次元素。
+选择排序在一趟排序中只是记录最大最小元素的位置，最后再交换元素
+"""
+
+def select_sort(lst):
+	for i in range(len(lst)):
+		min = lst[i]
+		index = i
+		for j in range(i+1, len(lst)):
+			if min > lst[j]:
+				min = lst[j]
+				index = j
+		lst[i], lst[index] = lst[index], lst[i]
+	return lst
 
 origin_list = [1,5,8,6,4,9,7,0]
 print(selectSort(origin_list))
